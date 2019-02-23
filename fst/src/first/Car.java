7e58a718e8cdc5bcc06bdd939ecaca003090b342
s19466 <s19466@lab4l-17.pjwstk.edu.pl>
2019-02-23 16:28:32 +0100

    test

--- fst/src/first/Car.java
+++ fst/src/first/Car.java
@@ -5,5 +5,27 @@ public class Car {
 	boolean isNew;
 	int milleage;
 	double price;
+	
+	void printCar(){
+		System.out.println("Marka: " + brand);
+		System.out.println("Nowy: " + isNew);
+		System.out.println("Przebieg: " + milleage);
+		System.out.println("Cena: " + price);
+	}
 
+	Car(){
+		System.out.println("Inicjacja z nowego konstruktora");
+	}
+	
+	Car(String brand){
+		this();
+		this.brand=brand;
+	}
+	
+	Car(String brand, boolean isNew, int milleage, double price){
+		this(brand);
+		this.isNew = isNew;
+		this.milleage = milleage;
+		this.price = price;
+	}
 }
--- fst/src/first/First.java
+++ fst/src/first/First.java
@@ -4,17 +4,8 @@ public class First {
 
 	public static void main(String[] args) {
 
-		Car car = new Car();
-		car.brand = "BMW";
-		car.isNew = true;
-		car.milleage = 0;
-		car.price = 100000.99;
-		
-		System.out.println("Marka: " + car.brand);
-		System.out.println("Nowy: " + car.isNew);
-		System.out.println("Przebieg: " + car.milleage);
-		System.out.println("Cena: " + car.price);
-
+		Car car = new Car("BMW", true, 0, 100000.99);
+		car.printCar();
 	}
 
 }
