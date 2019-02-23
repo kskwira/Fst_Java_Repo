f2f2d15239c17dda51cd9eef542e0b6812daa071
s19466 <s19466@lab4l-17.pjwstk.edu.pl>
2019-02-23 15:52:58 +0100

    test

---
+++ fst/src/first/Car.java
@@ -0,0 +1,9 @@
+package first;
+
+public class Car {
+	String brand;
+	boolean isNew;
+	int milleage;
+	double price;
+
+}
--- fst/src/first/First.java
+++ fst/src/first/First.java
@@ -3,7 +3,17 @@ package first;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.print("Ala ma kota");
+
+		Car car = new Car();
+		car.brand = "BMW";
+		car.isNew = true;
+		car.milleage = 0;
+		car.price = 100000.99;
+		
+		System.out.println("Marka: " + car.brand);
+		System.out.println("Nowy: " + car.isNew);
+		System.out.println("Przebieg: " + car.milleage);
+		System.out.println("Cena: " + car.price);
 
 	}
 
