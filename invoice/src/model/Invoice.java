6e5caab80b8fc59af19539fe469189ba3c3cbd4a
kskwira <chris.skwira@hotmail.co.uk>
2019-03-23 13:09:40 +0100

    comment

--- invoice/src/model/Invoice.java
+++ invoice/src/model/Invoice.java
@@ -10,8 +10,25 @@ public class Invoice {
 		return positions;
 	}
 	
+	private InvoicePosition positionWithProduct(Product product) {
+		for (InvoicePosition position: positions)
+		{
+			if (position.getProduct().getCode().equals(product.getCode()))
+				return position;
+		}
+		return null;
+	}
+	
 	public void addPosition(InvoicePosition position){
 		positions.add(position);
 	}
 
-}
+	public void addPosition(Product product) {
+		InvoicePosition position = positionWithProduct(product);
+		if (position == null) {
+			addPosition (new InvoicePosition(product));
+			return;
+		}
+		position.setCount(position.getCount()+1);
+		}
+	}
